a36e076f9b308c5eff1ddc2d688b3dc8325defdc
Leandro Henrique <leandro@lstech.com.br>
2015-08-27 13:54:44 -0300

    add Required if not

--- src/Validation/Validator.php
+++ src/Validation/Validator.php
@@ -6,6 +6,56 @@ use KennedyTedesco\Validation\Validator as TedescoValidator;
 
 class Validator extends TedescoValidator
 {
+    protected $addImplicitExtension = ['RequiredIfNot'];
+
+    protected function isImplicit($rule)
+    {
+        return in_array($rule, array_merge($this->addImplicitExtension, $this->implicitRules));
+    }
+
+    /**
+     * Validate that an attribute exists when another attribute has a given value.
+     *
+     * @param  string  $attribute
+     * @param  mixed   $value
+     * @param  mixed   $parameters
+     * @return bool
+     */
+    protected function validateRequiredIfNot($attribute, $value, $parameters)
+    {
+        $this->requireParameterCount(2, $parameters, 'required_if_not');
+
+        $data = Arr::get($this->data, $parameters[0]);
+
+        $values = array_slice($parameters, 1);
+
+        if (!in_array($data, $values)) {
+            return $this->validateRequired($attribute, $value);
+        }
+
+        return true;
+    }
+
+    /**
+     * Replace all place-holders for the required_if rule.
+     *
+     * @param  string  $message
+     * @param  string  $attribute
+     * @param  string  $rule
+     * @param  array   $parameters
+     * @return string
+     */
+    protected function replaceRequiredIfNot($message, $attribute, $rule, $parameters)
+    {
+        $values = implode(',', array_slice($parameters, 1));
+        $other  = $parameters[0];
+
+        $message = str_replace(':other', $other, $message);
+        $message = str_replace(':value', $values, $message);
+
+        return $message;
+    }
+
     /**
      * valida hora
      * @param  string $attribute
